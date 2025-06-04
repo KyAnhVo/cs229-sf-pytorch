da1ca4e6edfdfe3becb91d2fabd39d8c02ce12be
Ky Anh Vo <tommyvo0406@gmail.com>
2025-06-04 18:11:09 -0500

    changed notebook to py file

--- models/03.-svm.py
+++ models/03.-svm.py
@@ -8,7 +8,17 @@ m = 5000
 
 # Kernel function
 
-def gaussKernel(x1: torch.Tensor, x2: torch.Tensor, variance: float) -> float:
+def gaussianKernel(x1: torch.Tensor, x2: torch.Tensor, variance: float) -> float:
+    ''' returns the Gaussian (RBF) Kernel of the given column vectors
+    
+    Args:
+        x1 (torch.Tensor):  first column vector
+        x2 (torch.Tensor):  second column vector
+        variance (float):   variance of Kernel
+    Returns:
+        dot product of the two kernelized vectors of x1 and x2.
+
+    '''
     assert(x1.size()[1] == 1 and x2.size()[1] == 1), f'expect column vectors, received {x1.size()} and {x2.size()}'
     assert(x2.size() == x1.size()), f'expect vectors of equal length, received {x1.size()} and {x2.size()}'
 
@@ -17,3 +27,4 @@ def gaussKernel(x1: torch.Tensor, x2: torch.Tensor, variance: float) -> float:
     
     normSquared = (x1Flatten - x2Flatten).pow(2).sum()
     return torch.exp(-1 * normSquared / (2 * variance)).item()
+
